74d02c29b755453bc9331b3bd5e9b98b011581de
aaronh1986 <aaronharbourne@gmail.com>
2021-05-12 14:23:03 +0000

    14:50

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const SELECTIONS = [
+    {name: 'rock',
+     emoji: '✊',
+     beats: 'scissors'},
+     {name: 'paper',
+     emoji: '✋',
+     beats: 'rock'},
+     {name: 'scissors',
+     emoji: '✌',
+     beats: 'paper'},
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', (e) => {
       const selectionName = selectionButton.dataset.selection;
-      makeSelection(selectionName);
+      const selection = SELECTIONS.find(selection => selection.name === selectionName);
+      makeSelection(selection);
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection);
+    const computerSelection = randomSelection();
+    console.log(computerSelection);
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex];
 }
