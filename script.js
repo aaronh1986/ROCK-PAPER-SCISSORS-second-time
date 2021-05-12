39720f4017541b71c7ddc633b6c02f288d97a5b5
aaronh1986 <aaronharbourne@gmail.com>
2021-05-12 14:58:11 +0000

    Finished

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+const yourScoreSpan = document.querySelector('[data-your-score]');
 const SELECTIONS = [
     {name: 'rock',
      emoji: '✊',
@@ -23,11 +26,24 @@ function makeSelection(selection) {
     const computerSelection = randomSelection();
     const yourWinner = isWinner(selection, computerSelection);
     const computerWinner = isWinner(computerSelection, selection);
-    console.log(computerSelection);
+
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
+
+    if(yourWinner) incrementScore(yourScoreSpan);
+    if(computerWinner) incrementScore(computerScoreSpan);
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
 }
 
 function addSelectionResult(selection, winner) {
-    
+    const div = document.createElement('div');
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection');
+    if (winner) div.classList.add('winner');
+    finalColumn.after(div);
 }
 
 function isWinner(selection, opponentSelection) {
